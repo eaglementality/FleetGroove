2ae8253dcf0161c81139f0aecb01bcfdbcca8ba9
Jesse <97172881+eaglementality@users.noreply.github.com>
2025-09-02 10:43:33 +0000

    Update App.tsx

--- fleetGroove/src/App.tsx
+++ fleetGroove/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
       <h1 className="text-[80px] ">For sale</h1>
       <h5 className="text-[80px] font-bold">$800</h5>
       <p className="text-[80px]">Negotiable</p>
-      <div className="mt-2 text-lg">Location: Ghana, Accra. {'    '} <span>Contact: 0595545929</span></div>
+      <div className="mt-2 text-lg">Location: Ghana, Accra. {'    '} <span>Contact: 0595545929</span> <span>Email: jesseowusuyeboah99@gmail.com</span></div>
     </main>
   );
 }
